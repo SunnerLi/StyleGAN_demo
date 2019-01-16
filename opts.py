69f1fafa28398748ba2668e4803cce688e5cb051
SunnerLi <a6214123@gmail.com>
2019-01-16 15:38:22 +0800

    align the opt space

--- opts.py
+++ opts.py
@@ -17,12 +17,12 @@ def presentParameters(args_dict):
 class TrainOptions():
     def __init__(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument('--path', type = str, default = '/home/jameschengcs/sunner/Dataset/img_align_celeba')
-        parser.add_argument('--epoch', type = int, default = 10)
-        parser.add_argument('--batch_size', type = int, default = 32)
-        parser.add_argument('--type', type = str, default = 'style')
-        parser.add_argument('--resume', type = str, default = 'model.pth')
-        parser.add_argument('--det', type = str, default = 'train_result')
+        parser.add_argument('--path'        , type = str, default = '/home/jameschengcs/sunner/Dataset/img_align_celeba')
+        parser.add_argument('--epoch'       , type = int, default = 10)
+        parser.add_argument('--batch_size'  , type = int, default = 32)
+        parser.add_argument('--type'        , type = str, default = 'style')
+        parser.add_argument('--resume'      , type = str, default = 'model.pth')
+        parser.add_argument('--det'         , type = str, default = 'train_result')
         self.opts = parser.parse_args()
 
     def parse(self):
@@ -47,10 +47,10 @@ class TrainOptions():
 class InferenceOptions():
     def __init__(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument('--resume'  , type = str, default = 'train_result/model/latest.pth')
-        parser.add_argument('--type'    , type = str, default = 'style')
-        parser.add_argument('--num_face', type = int, default = 32)
-        parser.add_argument('--det'     , type = str, default = 'result.png')
+        parser.add_argument('--resume'      , type = str, default = 'train_result/model/latest.pth')
+        parser.add_argument('--type'        , type = str, default = 'style')
+        parser.add_argument('--num_face'    , type = int, default = 32)
+        parser.add_argument('--det'         , type = str, default = 'result.png')
         self.opts = parser.parse_args()
 
     def parse(self):
